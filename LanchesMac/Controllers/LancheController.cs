3469c291a0b39ae5c1410016a21be32ef356b3de
Alexandre Kawahara <akawahara2@terra.com.br>
2024-11-26 20:30:35 -0300

    Projeto LanchesMac

---
+++ LanchesMac/Controllers/LancheController.cs
@@ -0,0 +1,27 @@
+﻿using LanchesMac.Repositories.Interfaces;
+using LanchesMac.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class LancheController : Controller
+    {
+        private ILancheRepository _lancherepository;
+
+        public LancheController(ILancheRepository lancherepository)
+        {
+            _lancherepository = lancherepository;
+        }
+
+        public IActionResult List()
+        {
+            //var lanches = _lancherepository.Lanches;
+            //return View(lanches);
+
+            var lanchesListViewModel = new LancheListViewModel();
+            lanchesListViewModel.Lanches = _lancherepository.Lanches;
+            lanchesListViewModel.CategoriaAtual = "Categoria Atual";
+            return View(lanchesListViewModel);
+        }
+    }
+}
